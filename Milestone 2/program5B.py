2d0904ea9778b4e632075d248c314ed4066c14b5
truman <trumanjimothy@gmail.com>
2024-11-11 10:02:34 -0500

    Milestone 2

---
+++ Milestone 2/program5B.py
@@ -0,0 +1,37 @@
+from typing import List, Tuple
+
+    
+def program5B(n: int, W: int, heights: List[int], widths: List[int]) -> Tuple[int, int, List[int]]:
+    """
+    Solution to Program 5B
+    
+    Parameters:
+    n (int): number of paintings
+    W (int): width of the platform
+    heights (List[int]): heights of the paintings
+    widths (List[int]): widths of the paintings
+
+    Returns:
+    int: number of platforms used
+    int: optimal total height
+    List[int]: number of paintings on each platform
+    """
+    ############################
+    # Add you code here
+    ############################
+
+    return 0, 0, [] # replace with your code
+
+
+if __name__ == '__main__':
+    n, W = map(int, input().split())
+    heights = list(map(int, input().split()))
+    widths = list(map(int, input().split()))
+
+    m, total_height, num_paintings = program5B(n, W, heights, widths)
+
+    print(m)
+    print(total_height)
+    for i in num_paintings:
+        print(i)
+    
